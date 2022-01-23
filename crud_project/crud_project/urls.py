8e3231c4b269d136042ba087614a3bb301154ec1
ma.orojloo <ma.orojloo@gmail.com>
2022-01-23 15:17:26 +0330

    editing some tmeps ui

--- crud_project/crud_project/urls.py
+++ crud_project/crud_project/urls.py
@@ -11,7 +11,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 
-urlpatterns = [
+urlpatterns = [ 
     path('password-reset/',  auth_views.PasswordResetView.as_view(template_name='users/resetpassword.html'), name='password_reset'),
     path('password-reset-done/',  auth_views.PasswordResetDoneView.as_view(template_name='users/resetpassword-done.html'), name='password_reset_done'),
     path('password-reset-confirm/<uidb64>/<token>/',  auth_views.PasswordResetConfirmView.as_view(template_name='users/PasswordReset-Confirm.html'), name='password_reset_confirm'),
